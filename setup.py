e72403e5d0754b31561bb959baefecdef12c3d4a
Ondřej Caletka <ondrej@caletka.cz>
2018-07-07 22:55:35 +0200

    Pytest integration

--- setup.py
+++ setup.py
@@ -2,18 +2,19 @@ from setuptools import setup
 
 
 setup(
-        name="dzonegit",
-        version="0.1",
-        description="Git hooks to admin DNS zone files in git",
-        author="Ondřej Caletka",
-        author_email="ondrej@caletka.cz",
-        license="MIT",
-        py_modules=["dzonegit"],
-        entry_points={
+    name="dzonegit",
+    version="0.1",
+    description="Git hooks to admin DNS zone files in git",
+    author="Ondřej Caletka",
+    author_email="ondrej@caletka.cz",
+    license="MIT",
+    py_modules=["dzonegit"],
+    setup_requires=['pytest-runner', ],
+    tests_require=['pytest', ],
+    entry_points={
             "console_scripts": [
                 "dzonegit = dzonegit:main",
                 "pre-commit = dzonegit:pre_commit",
-                ],
-            },
+            ],
+    },
 )
-
