05eec2378038e3feda7a3300df3c6c3de36b98cb
Ondřej Caletka <ondrej@caletka.cz>
2018-07-12 14:43:46 +0200

    Add minimal required python version and classifiers

--- setup.py
+++ setup.py
@@ -9,12 +9,26 @@ setup(
     author_email="ondrej@caletka.cz",
     license="MIT",
     py_modules=["dzonegit"],
-    setup_requires=['pytest-runner', ],
-    tests_require=['pytest', ],
+    setup_requires=["pytest-runner"],
+    python_requires=">=3.5",
+    tests_require=["pytest"],
     entry_points={
             "console_scripts": [
                 "dzonegit = dzonegit:main",
                 "dzonegit-pre-commit = dzonegit:pre_commit",
             ],
     },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: Version Control :: Git",
+        "Topic :: System :: Systems Administration",
+    ],
+
 )
